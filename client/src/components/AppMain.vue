6d4d97458b74fa5a73faec0b99a69cd85ca127b6
FabioBabaglioni <fabiobabaglioni@gmail.com>
2023-01-13 12:14:06 +0100

    Creo la chiamata axios per collegare front e back

---
+++ client/src/components/AppMain.vue
@@ -0,0 +1,42 @@
+<script >
+
+// importo axios
+import axios from "axios"
+
+const apiUrl = "http://localhost:8888/"
+
+export default {
+    data() {
+        return {
+            toDoList: [],
+            newTodoList: ""
+        };
+    },
+    methods() {
+        getAllData(){
+            axios.get(apiUrl + "api.php")
+                .then(res => {
+                    const data = res.data
+
+                    console.log(data);
+                })
+        }
+    },
+    mounted() {
+        getAllData()
+    }
+}
+
+</script>
+
+<template>
+
+    <main>
+
+    </main>
+
+</template>
+
+<style scoped>
+
+</style>
